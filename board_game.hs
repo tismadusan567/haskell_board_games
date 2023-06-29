25e70360935210f2db23669ed9f9f59dcc7bcbac
Dusan Tisma <tismadusan567@gmail.com>
2023-06-29 04:42:29 +0200

    isFInished and validMoves

--- board_game.hs
+++ board_game.hs
@@ -1,3 +1,4 @@
+import Data.List (transpose)
 data Rose a = Node a [Rose a]
 
 -- 1. Struktura podataka Rose tree (10p)
@@ -47,34 +48,84 @@ test = Node 1 [Node 2 [Node 3 []], Node 4 [], Node 5 [Node 6 [], Node 7 []], Nod
 
 -- 2. Predstavljanje stanja u igri (10p)
 
-data Player = First | Second
+data Player = First | Second deriving (Show)
 
 data BoardState a = BoardState Player [[a]]
 
-data GameMove = GameMove Player (Int, Int)
+data GameMove = GameMove Player (Int, Int) deriving (Show)
 
-newtype GameStateOp s a = GameStateOp { runState :: BoardState s -> (a,BoardState s) }
+-- GameStateOp
+
+newtype GameStateOp s a = GameStateOp { runGameState :: BoardState s -> (a,BoardState s) }
 
 instance Functor (GameStateOp s) where
     fmap :: (a -> b) -> GameStateOp s a -> GameStateOp s b
-    fmap f (GameStateOp op) = GameStateOp $ \state -> let (a, newState) = op state in (f a, newState) 
-    
+    fmap f (GameStateOp op) = GameStateOp $ \state -> let (a, newState) = op state in (f a, newState)
+
 instance Applicative (GameStateOp s) where
     pure :: a -> GameStateOp s a
     pure x = GameStateOp $ \state -> (x, state)
-    
+
     (<*>) :: GameStateOp s (a -> b) -> GameStateOp s a -> GameStateOp s b
     (GameStateOp op1) <*> (GameStateOp op2) = GameStateOp $ \state ->
         let (f, state1) = op1 state
             (res, state2) = op2 state1
         in (f res, state2)
 
-instance Monad (GameStateOp s) where 
+instance Monad (GameStateOp s) where
     return :: a -> GameStateOp s a
     return = pure
     (>>=) :: GameStateOp s a -> (a -> GameStateOp s b) -> GameStateOp s b
-    (GameStateOp op) >>= f = GameStateOp $ \state -> 
+    (GameStateOp op) >>= f = GameStateOp $ \state ->
         let (a, newState) = op state
             (GameStateOp newOp) = f a
         in newOp newState
 
+-- GameStateOpHistory
+
+newtype GameStateOpHistory s a = GameStateOpHistory { runGameStateH :: [BoardState s] -> (a,[BoardState s]) }
+
+instance Functor (GameStateOpHistory s) where
+    fmap :: (a -> b) -> GameStateOpHistory s a -> GameStateOpHistory s b
+    fmap f (GameStateOpHistory op) = GameStateOpHistory $ \states -> let (a, newStates) = op states in (f a, newStates)
+
+instance Applicative (GameStateOpHistory s) where
+    pure :: a -> GameStateOpHistory s a
+    pure x = GameStateOpHistory $ \states -> (x, states)
+
+    (<*>) :: GameStateOpHistory s (a -> b) -> GameStateOpHistory s a -> GameStateOpHistory s b
+    (GameStateOpHistory op1) <*> (GameStateOpHistory op2) = GameStateOpHistory $ \states ->
+        let (f, states1) = op1 states
+            (res, states2) = op2 states1
+        in (f res, states2)
+
+instance Monad (GameStateOpHistory s) where
+    return :: a -> GameStateOpHistory s a
+    return = pure
+
+    (>>=) :: GameStateOpHistory s a -> (a -> GameStateOpHistory s b) -> GameStateOpHistory s b
+    (GameStateOpHistory op) >>= f = GameStateOpHistory $ \states ->
+        let (a, newStates) = op states
+            (GameStateOpHistory newOp) = f a
+        in newOp newStates
+
+-- 3. Primena kreiranih tipova na primeru igre Iks-Oks (12p)
+
+data XOPolje = X | O | P deriving (Eq)
+
+validMoves :: BoardState XOPolje -> [GameMove]
+validMoves (BoardState player board) = [GameMove player (rowIndex, colIndex) |
+    (rowIndex, row) <- zip [0..] board,
+    (colIndex, el) <- zip [0..] row,
+    el == P]
+
+isFinished :: BoardState XOPolje -> Bool
+isFinished (BoardState _ board) = 
+    P `notElem` concat board 
+    || any checkSeq board
+    || any checkSeq (transpose board)
+    || checkSeq [el | (rowIndex, row) <- zip [0..] board, (colIndex, el) <- zip [0..] row, rowIndex == colIndex]
+    || checkSeq [el | (rowIndex, row) <- zip [0..] board, (colIndex, el) <- zip [0..] row, rowIndex + colIndex + 1 == length row]
+    where checkSeq seq = all (X==) seq || all (O==) seq
+
+testState = BoardState First [[P,P,P],[X,P,O],[P,X,P]]
